17b05f484207492f21a0b359ee890c87c3f16aa2
antipas <entipasengapono@icloud.com>
2025-05-06 14:15:07 +0100

    ajout du contenu dans acceuilpage

---
+++ lib/pages/acceuil/acceuil_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AcceuilPage extends StatefulWidget {
+  const AcceuilPage({super.key});
+
+  @override
+  State<AcceuilPage> createState() => _AcceuilPageState();
+}
+
+class _AcceuilPageState extends State<AcceuilPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
