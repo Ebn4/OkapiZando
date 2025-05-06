7f119c40df5ff03ad617eace0f314664a77077b3
antipas <entipasengapono@icloud.com>
2025-05-06 21:47:16 +0100

    ajout du contenu dans acceuil

--- lib/pages/acceuil/acceuil_page.dart
+++ lib/pages/acceuil/acceuil_page.dart
@@ -13,14 +13,19 @@ class _AcceuilPageState extends State<AcceuilPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Image.asset('assets/images/Okapizando.png',width: 100,height: 100,),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          'assets/images/Okapizando.png',
+          width: 100,
+          height: 100,
+        ),
         actions: [
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
-                
                 padding: const EdgeInsets.only(right: 15),
                 child: badges.Badge(
                   badgeContent: Text(
@@ -53,6 +58,199 @@ class _AcceuilPageState extends State<AcceuilPage> {
             ],
           ),
         ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Latest',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 160,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 300,
+                    height: double.infinity,
+                    color: Colors.red,
+                    child: Center(child: const Text('un')),
+                  ),
+                  SizedBox(width: 15),
+                  Container(
+                    width: 300,
+                    height: double.infinity,
+                    color: Colors.blue,
+                    child: Center(child: const Text('un')),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Categories',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            SizedBox(
+              height: 110,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(Icons.shopping_bag, size: 30),
+                      ),
+                      SizedBox(height: 10),
+                      const Text('un'),
+                    ],
+                  ),
+                  SizedBox(width: 15),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(Icons.shopping_bag, size: 30),
+                      ),
+                      SizedBox(height: 10),
+                      const Text('un'),
+                    ],
+                  ),
+                  SizedBox(width: 15),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Colors.lightGreen,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(Icons.shopping_bag, size: 30),
+                      ),
+                      SizedBox(height: 10),
+                      const Text('un'),
+                    ],
+                  ),
+                  SizedBox(width: 15),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(Icons.skip_next, size: 30),
+                      ),
+                      SizedBox(height: 10),
+                      const Text('un'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              color: Colors.blue,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text(
+                  'Nous vous offrond des services et des ppproduits de qualité et abordable',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Meilleurs',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 8),
+                 const Text(
+                  'ventes',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Meilleurs',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 8),
+                 const Text(
+                  'ventes',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
