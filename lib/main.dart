c086f69dcffe0264bd81db6d1b2b82759ee27cf8
antipas <entipasengapono@icloud.com>
2025-05-09 16:07:50 +0100

    modifiation  de main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:okapi_zando_mobile/pages/widgets/accueil.dart';
-import 'package:okapi_zando_mobile/pages/widgets/singinOrginup.dart';
-=======
-
-import 'package:okapi_zando_mobile/pages/widgets/accueil.dart';
-
->>>>>>> e3f12af9be308fa106bcfe0063c31d28579bc461
-
-
 void main() {
   runApp(const MyApp());
 }
