b73fee5d59e6a6bcc2314a6babfb45a2d92ba7e5
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-10 18:45:28 +0200

    Ajout du fichier .env qui contient l'url de l'api

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:okapi_zando_mobile/pages/acceuil/acceuilPage.dart';
 import 'package:okapi_zando_mobile/pages/widgets/accueil.dart';
 import 'package:okapi_zando_mobile/pages/widgets/introductionPage.dart';
 import 'package:okapi_zando_mobile/pages/userProfile/profilePage.dart';
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+void main() async{
+  await dotenv.load(); // Charge le fichier .env
   runApp(const MyApp());
 }
 
