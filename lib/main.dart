ccafa0632e96e7dbe02e67b41b438e15899b3d58
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-16 11:09:39 +0200

    Ajout de get_it pour l'injection de dependance et riiverpod pour le stateManagement

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:okapi_zando_mobile/business/service/zandoNetworkService.dart';
+import 'package:okapi_zando_mobile/framework/zandoNetworkServiceImpl.dart';
 import 'package:okapi_zando_mobile/pages/acceuil/acceuilPage.dart';
 import 'package:okapi_zando_mobile/pages/detailProduit/detailProduitPage.dart';
 import 'package:okapi_zando_mobile/pages/widgets/accueil.dart';
 import 'package:okapi_zando_mobile/pages/widgets/introductionPage.dart';
 import 'package:okapi_zando_mobile/pages/userProfile/profilePage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final getIt = GetIt.instance;
+
+void setup(){
+  getIt.registerLazySingleton<ZandoNetworkService>(() {
+    return Zandonetworkserviceimpl();
+  });
+}
 
 void main() async{
   await dotenv.load(); // Charge le fichier .env
+  runApp(ProviderScope(child: MyApp()));
 }
 
 // co
