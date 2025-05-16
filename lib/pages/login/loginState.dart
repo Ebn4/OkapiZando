195e2b0edf7886d11e6f5451409087d9d4b7d853
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-16 11:08:41 +0200

    L'implementation de la classe loginState

--- lib/pages/login/loginState.dart
+++ lib/pages/login/loginState.dart
@@ -1,3 +1,18 @@
-class Loginstate {
-  
+import 'package:okapi_zando_mobile/business/models/user.dart';
+
+class LoginState {
+  bool? isLoading;
+  String? errorMsg;
+  User? user;
+
+  // le constructeur
+  LoginState({this.isLoading, this.errorMsg, this.user});
+
+  LoginState copywith({bool? isLoading, String? errorMsg, User? user}) {
+    return LoginState(
+      isLoading: isLoading ?? this.isLoading,
+      errorMsg: errorMsg ?? this.errorMsg,
+      user: user ?? this.user
+    );
+  }
 }
