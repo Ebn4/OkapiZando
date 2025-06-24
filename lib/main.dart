49623b6bd1ad7acab0e5699046cdc8ca69d20c77
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-24 22:38:10 +0200

    Injection de la dependance des methodes pour la gestion local de l'utilisateur

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:okapi_zando_mobile/business/service/zandoLocalService.dart';
 import 'package:okapi_zando_mobile/business/service/zandoNetworkService.dart';
+import 'package:okapi_zando_mobile/framework/zandoLocalServiceImpl.dart';
 import 'package:okapi_zando_mobile/framework/zandoNetworkServiceImpl.dart';
 import 'package:okapi_zando_mobile/pages/acceuil/acceuilPage.dart';
 import 'package:okapi_zando_mobile/pages/detailProduit/detailProduitPage.dart';
@@ -9,6 +11,7 @@ import 'package:okapi_zando_mobile/pages/singin/signinPage.dart';
 import 'package:okapi_zando_mobile/pages/widgets/accueil.dart';
 import 'package:okapi_zando_mobile/pages/widgets/introductionPage.dart';
 import 'package:okapi_zando_mobile/pages/userProfile/profilePage.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,10 +23,16 @@ void setup() {
   getIt.registerLazySingleton<ZandoNetworkService>(() {
     return Zandonetworkserviceimpl();
   });
+
+  // initialisation de l'injection de dependance en local
+  getIt.registerLazySingleton<ZandoLocalService>(() {
+    return ZandoLocalServiceImpl(box: GetStorage());
+  });
 }
 
 void main() async {
   await dotenv.load(); // Charge le fichier .env
+  await GetStorage.init(); // Initialise GetStorage pour la gestion de stockage local
   setup(); // appelle de la methode pour initialisé get_it
   runApp(ProviderScope(child: MyApp()));
 }
